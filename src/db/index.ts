4d76c172d38587a62f31a1e95905bbd4b4fa8a77
Victory Gold <gvictory3434@gmail.com>
2026-08-11 16:43:37 +0100

    design database schemas

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,2 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined');
-}
-
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql);
+export * from "./schema/app";
+export * from "./schema/auth";
